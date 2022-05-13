e435a13f56df7a1dfe4cd25fb29086879ff1c667
Kimiya Kitani <kimipooh@gmail.com>
2022-05-13 23:02:44 +0900

    1.0 released.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # urldecodecopy
-エンコード前のURLをクリップボードにコピーする Google Chrome用の拡張機能です
+エンコード前のURLをクリップボードにコピーする Google Chrome用の拡張機能です。
+Google Help Community での回答用に、Chromeの拡張機能に詳しい Product Experts の力を借りて作成しました。
+
+# インストール方法
+ダウンロード（ZIP圧縮）し、そのファイルを展開。
+Chormeの拡張機能にある「パッケージ化されていな拡張機能を読み込む」より、展開したフォルダを選択
---
+++ popup.js
@@ -0,0 +1,95 @@
+document.querySelector('#PopupCopyUrlA').addEventListener('click', async (event) => {
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, tabs => 
+  {
+    var url = tabs[0].url;
+
+    if( (new RegExp('https:\/\/*')).test(url) )
+    {
+      chrome.scripting.executeScript(
+        {
+          target:{tabId: tabs[0].id},
+          func: CopyUrl,
+          args: [url]
+        },
+        notifyResults
+      );
+    }
+  });
+});
+
+document.querySelector('#PopupCopyUrlB').addEventListener('click', async (event) => {
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, tabs => 
+  {
+    var url = tabs[0].url;
+
+    if( (new RegExp('https:\/\/*')).test(url) )
+    {
+      chrome.scripting.executeScript(
+        {
+          target:{tabId: tabs[0].id},
+          func: CopyUrl2,
+          args: [url]
+        },
+        notifyResults
+      );
+    }
+  });
+});
+
+function notifyResults(results) {
+  var res = results[0].result;
+  let opt = {
+    type: "basic",
+    title: "URLコピー成功",
+    message: res,
+    iconUrl: "icon_48x48.png"
+  };
+  chrome.notifications.create(opt);
+  window.close();
+}
+
+function CopyUrl(url){
+  let decodeURL = "";
+  try{
+    decodeURL = decodeURIComponent(url);
+  }catch(e){}
+  let elem = document.createElement( 'a' );
+  elem.href = url;
+  elem.appendChild(document.createTextNode(decodeURL));
+  document.body.appendChild(elem);
+  var range = document.createRange();
+  range.selectNode(elem);
+  var selection = document.getSelection();
+  selection.removeAllRanges();
+  selection.addRange(range);
+  document.execCommand('copy');
+  document.getSelection().removeAllRanges();
+  document.body.removeChild(elem);
+  return url;
+}
+
+function CopyUrl2(url){
+  let decodeURL = "";
+  try{
+    decodeURL = decodeURIComponent(url);
+  }catch(e){}
+  let get_tag = "h1";
+  if( (new RegExp('https:\/\/kitaney-google\.blogspot\.com\/*')).test(url) ){
+      get_tag = "h3";
+  }
+  let title = document.querySelector(get_tag).innerText;
+  let elem = document.createElement( 'a' );
+  elem.href = url;
+  elem.appendChild(document.createTextNode(title));
+  document.body.appendChild(elem);
+  var range = document.createRange();
+  range.selectNode(elem);
+  var selection = document.getSelection();
+  selection.removeAllRanges();
+  selection.addRange(range);
+  document.execCommand('copy');
+  document.getSelection().removeAllRanges();
+  document.body.removeChild(elem);
+  return title;
+}
+
